3ece8e7c50da5a72e246baccd8feaa11ea7e01e4
Ciara <147958656+ThirdMorrigan@users.noreply.github.com>
2026-08-28 19:33:28 +0100

    background visuals pass 1

---
+++ background.gd
@@ -0,0 +1,14 @@
+extends TextureRect
+@export var parallax := 0.1
+@export var cam :Camera2D
+func _ready() -> void:
+	var resize : Callable = (func():
+		var vp=get_viewport_rect()
+		texture.width=vp.size.x
+		texture.height=vp.size.y)
+	resize.call_deferred()
+	get_viewport().size_changed.connect(resize,CONNECT_DEFERRED)
+func _process(delta: float) -> void:
+	var cam_offset = cam.position * parallax
+	texture.noise.offset=Vector3(cam_offset.x,cam_offset.y,0.)
+	FastNoiseLite
--- ball_cam.gd
+++ ball_cam.gd
@@ -1,13 +1,19 @@
 extends Camera2D
 class_name Cam
 @export var ball : Ball
+
+@onready var background: TextureRect = $background
+
 var zoom_level : float = 1. : 
 	set(z):
 		z = clamp(z,0.25,2.)
 		zoom_level=z
 		zoom=Vector2(z,z)
-func _ready() -> void: global_position=ball.global_position
-func _process(delta: float) -> void:global_position = lerp(global_position,ball.global_position,delta*4.)
+func _ready() -> void:
+	offset=ball.global_position
+func _process(delta: float) -> void:
+	offset = lerp(offset,ball.global_position,delta*4.)
+	
 func _unhandled_key_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_up"):
 		zoom_level += 0.25
