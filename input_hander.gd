43aee9ba73437924a9702dfa1186ee1fbc317474
Nathan <poppyandnunubot@gmail.com>
2026-08-29 00:50:10 +0100

    Added basic dotted line visual

--- input_hander.gd
+++ input_hander.gd
@@ -4,23 +4,29 @@ extends Node
 
 @onready var visual: Node2D = $"../visual"
 @onready var moon: Ball = $".."
+@onready var line_2d: Line2D = $"../Line2D"
 
 
 var mouseDown : bool = false
 var mouseInitialPostision : Vector2
+var mouseCurrentPosition : Vector2
 var mouseToMoonDirection : Vector2
 var magnituge : float
 
+const LINE_LENGTH : float = 3
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	line_2d.add_point(Vector2.ZERO)
+	line_2d.add_point(Vector2.ZERO)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if mouseDown:
-		magnituge = minf(500, abs(mouseInitialPostision-get_viewport().get_mouse_position()).length())
+		magnituge = minf(500, abs(mouseInitialPostision-mouseCurrentPosition).length())
 		visual.wobble_strength = magnituge/50
+		line_2d.set_point_position(1,(moon.position-mouseCurrentPosition).normalized() * magnituge * LINE_LENGTH)
 
 func _input(event):
 	if not moon.shootable:
@@ -32,12 +38,16 @@ func _input(event):
 		if event.pressed && event.button_index == MOUSE_BUTTON_LEFT:
 			mouseDown = true
 			mouseInitialPostision = event.position
+			mouseCurrentPosition = mouseInitialPostision
 			
 		elif mouseDown && event.button_index == MOUSE_BUTTON_LEFT:
 			moon.add_velocity((moon.position-event.position).normalized() * magnituge)
 			moon.shootable = false
 			mouseDown = false
 			visual.wobble_strength = 0
-			
+			line_2d.set_point_position(1,Vector2.ZERO)
+	elif event is InputEventMouseMotion:
+		event.position = camera.get_global_mouse_position()
+		mouseCurrentPosition = event.position
 	
 		
