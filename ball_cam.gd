ad4d03c28ac80e19b4a0606865b1cb4f8443f028
Nathan <poppyandnunubot@gmail.com>
2026-08-29 17:54:28 +0100

    Added lerp to camera zoom

--- ball_cam.gd
+++ ball_cam.gd
@@ -1,17 +1,19 @@
 extends Camera2D
 class_name Cam
 @export var ball : Ball
-
+@export var zoom_speed : float = 2.0
 
 var zoom_level : float = 1. : 
 	set(z):
 		z = clamp(z,0.25,2.)
 		zoom_level=z
-		zoom=Vector2(z,z)
 func _ready() -> void:
 	offset=ball.global_position
+	
 func _process(delta: float) -> void:
 	offset = lerp(offset,ball.global_position,delta*4.)
+	var im_just_lerpin = lerp(zoom.x,zoom_level,delta * zoom_speed)
+	zoom = Vector2(im_just_lerpin,im_just_lerpin)
 	
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action("ui_up"):
