be550d9cdf59103836cae37605f179fc7b0cda06
neopetal <jtrollemd@gmail.com>
2026-08-29 16:40:01 +0100

    woosh sound

---
+++ sound.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@onready var audio_player:AudioStreamPlayer2D = $AudioStreamPlayer2D
+@onready var audio_player_2:AudioStreamPlayer2D = $AudioStreamPlayer2D2
+@onready var planet:Ball = $".."
+
+var velocity_sound_volume = 0.5
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var vel = remap(planet.velocity.length(), 0, 400, 0.2, 2)
+	var vel2 = remap(planet.velocity.length_squared(), 0, 300000, 0.1, 6)
+	var vol = remap(planet.velocity.length(), 100, 300, 0.2, 1) * velocity_sound_volume
+	audio_player.pitch_scale = vel
+	audio_player_2.pitch_scale = vel2
+	audio_player.volume_linear = vol
+	audio_player_2.volume_linear = vol * 0.3
+	pass
