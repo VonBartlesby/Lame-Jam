218991d4ee60d56f0d5552da2467bd82a8072a82
Nathan <poppyandnunubot@gmail.com>
2026-08-29 18:45:43 +0100

    Added lerp to sound effects to avoid sudden spikes

--- sound.gd
+++ sound.gd
@@ -6,6 +6,10 @@ extends Node2D
 
 var velocity_sound_volume = 0.5
 
+var pitch_scale_target : float
+var pitch_scale_2_target : float
+var volume_scale_target : float
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	audio_player.volume_linear = 0
@@ -15,13 +19,13 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if planet.velocity.length() > 0.5:
-		var vel = remap(planet.velocity.length(), 0, 400, 0.2, 2)
-		var vel2 = remap(planet.velocity.length_squared(), 0, 300000, 0.1, 6)
-		var vol = remap(planet.velocity.length(), 100, 300, 0.2, 1) * velocity_sound_volume
-		audio_player.pitch_scale = vel
-		audio_player_2.pitch_scale = vel2
-		audio_player.volume_linear = maxf(vol,0)
-		audio_player_2.volume_linear = maxf(vol * 0.3,0)
+		pitch_scale_target = remap(planet.velocity.length(), 0, 400, 0.2, 2)
+		pitch_scale_2_target = remap(planet.velocity.length_squared(), 0, 300000, 0.1, 6)
+		volume_scale_target = remap(planet.velocity.length(), 100, 300, 0.2, 1) * velocity_sound_volume
+		audio_player.pitch_scale = lerp(audio_player.pitch_scale,pitch_scale_target,delta)
+		audio_player_2.pitch_scale = lerp(audio_player_2.pitch_scale,pitch_scale_2_target,delta)
+		audio_player.volume_linear = maxf(lerp(audio_player.volume_linear,volume_scale_target,delta),0)
+		audio_player_2.volume_linear = maxf(lerp(audio_player.volume_linear,volume_scale_target,delta) * 0.3,0)
 	else:
 		audio_player.volume_linear = 0
 		audio_player_2.volume_linear = 0
