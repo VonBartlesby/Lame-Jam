79aa6b08d4348b75fef46b53a7ff5e90174d49c8
neopetal <jtrollemd@gmail.com>
2026-08-30 01:16:36 +0100

    woosh sound (rumble layer)

--- Scripts/sound.gd
+++ Scripts/sound.gd
@@ -1,22 +1,26 @@
 extends Node2D
 
-@onready var audio_player:AudioStreamPlayer2D = $AudioStreamPlayer2D
-@onready var audio_player_2:AudioStreamPlayer2D = $AudioStreamPlayer2D2
+@onready var woosh_sound:AudioStreamPlayer2D = $woosh
+@onready var woosh_high_sound:AudioStreamPlayer2D = $"woosh high"
+@onready var woosh_rumble_sound:AudioStreamPlayer2D = $"woosh rumble"
 @onready var impact_sound:AudioStreamPlayer2D = $impact
 @onready var reflect_sound:AudioStreamPlayer2D = $reflect
 @onready var launch_sound:AudioStreamPlayer2D = $launch
 @onready var planet:Ball = $".."
 
-var velocity_sound_volume = 0.5
+var velocity_sound_volume = 0.2
 
 var pitch_scale_target : float
 var pitch_scale_2_target : float
 var volume_scale_target : float
+var volume_scale_target_rumble : float
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	audio_player.volume_linear = 0
-	audio_player_2.volume_linear = 0
+	woosh_sound.volume_linear = 0
+	woosh_high_sound.volume_linear = 0
+	woosh_rumble_sound.volume_linear = 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,13 +29,16 @@ func _process(delta: float) -> void:
 		pitch_scale_target = remap(planet.velocity.length(), 0, 400, 0.2, 2)
 		pitch_scale_2_target = remap(planet.velocity.length_squared(), 0, 300000, 0.1, 6)
 		volume_scale_target = remap(planet.velocity.length(), 100, 300, 0.2, 1) * velocity_sound_volume
-		audio_player.pitch_scale = lerp(audio_player.pitch_scale,pitch_scale_target,delta)
-		audio_player_2.pitch_scale = lerp(audio_player_2.pitch_scale,pitch_scale_2_target,delta)
-		audio_player.volume_linear = maxf(lerp(audio_player.volume_linear,volume_scale_target,delta),0)
-		audio_player_2.volume_linear = maxf(lerp(audio_player.volume_linear,volume_scale_target,delta) * 0.3,0)
+		volume_scale_target_rumble = remap(planet.velocity.length(), 200, 700, 0.1, 1) * velocity_sound_volume
+		woosh_sound.pitch_scale = lerp(woosh_sound.pitch_scale,pitch_scale_target,delta)
+		woosh_high_sound.pitch_scale = lerp(woosh_high_sound.pitch_scale,pitch_scale_2_target,delta)
+		woosh_sound.volume_linear = maxf(lerp(woosh_sound.volume_linear,volume_scale_target,delta),0)
+		woosh_high_sound.volume_linear = maxf(lerp(woosh_sound.volume_linear,volume_scale_target,delta) * 0.3,0)
+		woosh_rumble_sound.volume_linear = maxf(lerp(woosh_rumble_sound.volume_linear,volume_scale_target_rumble,delta),0)
 	else:
-		audio_player.volume_linear = 0
-		audio_player_2.volume_linear = 0
+		woosh_sound.volume_linear = 0
+		woosh_high_sound.volume_linear = 0
+		woosh_rumble_sound.volume_linear = 0
 
 
 func _on_our_moon_impacted() -> void:
