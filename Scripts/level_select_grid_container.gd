8b855d15a22459642a979e7f4feea1768b2c96c6
Nathan <poppyandnunubot@gmail.com>
2026-08-31 00:19:41 +0100

    Added new font and text colours

--- Scripts/level_select_grid_container.gd
+++ Scripts/level_select_grid_container.gd
@@ -1,6 +1,7 @@
 extends GridContainer
 
 var levels = GameControllerAutoLoad.levels
+const GAME_THEME = preload("uid://c7iw7wjbt70ov")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,6 +10,8 @@ func _ready() -> void:
 		var button = LevelSelectButton.new()
 		button.text = "Level %d" % [count + 1]
 		button.level_index = count
+		button.theme = GAME_THEME
+		button.add_theme_font_size_override("font size",8)
 		add_child(button)
 		count += 1
 
