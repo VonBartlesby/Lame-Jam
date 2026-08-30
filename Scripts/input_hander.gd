af5e546a1958db6b0116c530bdb3eeb886cda2a9
neopetal <jtrollemd@gmail.com>
2026-08-30 21:39:40 +0100

    mouse is captured when u charging a shot now

--- Scripts/input_hander.gd
+++ Scripts/input_hander.gd
@@ -6,6 +6,8 @@ extends Node
 @onready var visual: Node2D = $"../visual"
 @onready var moon: Ball = $".."
 @onready var line_2d: Line2D = $"../Line2D"
+@onready var charge: Line2D = $"../charge"
+
 @onready var path_finder: Node2D = $"../Path Finder"
 
 @onready var charge_noise: AudioStreamPlayer = $AudioStreamPlayer
@@ -41,32 +43,42 @@ func _unhandled_input(event):
 		return
 	if event is InputEventMouse:
 		event.position = camera.get_global_mouse_position()
-		moon_launch_velocity = (moon.position-event.position).normalized() * magnituge
+		moon_launch_velocity = mouseCurrentPosition.normalized() * magnituge
+		
 	if event is InputEventMouseButton:
 		
 		
 		if event.pressed && event.button_index == MOUSE_BUTTON_LEFT:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 			mouseDown = true
-			mouseInitialPostision = event.position
+			mouseInitialPostision = Vector2(0,0)
 			mouseCurrentPosition = mouseInitialPostision
 			
+			
 		elif mouseDown && event.button_index == MOUSE_BUTTON_LEFT:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 			moon.shootable = false
 			moon.started = true
 			moon.charge = 0
 			moon.add_velocity(moon_launch_velocity,false)
+
 			SignalHandler.launched.emit()
 			
 			mouseDown = false
 			visual.wobble_strength = 0
 			line_2d.clear_points()
+			charge.visible = false
 	elif event is InputEventMouseMotion:
-		mouseCurrentPosition = event.position
+		mouseCurrentPosition -= (event.screen_relative * 0.8)
+		
+		charge.set_point_position(1,-mouseCurrentPosition )
 		if mouseDown:
 			line_2d.clear_points()
+			charge.visible = false
 			var points = path_finder.launch(moon_launch_velocity)
-			magnituge = minf(500, abs(mouseInitialPostision-mouseCurrentPosition).length())
+			magnituge = minf(500, abs(mouseCurrentPosition).length())
 			visual.wobble_strength = magnituge/WOBBLE_RATIO
+			charge.visible = true
 			#line_2d.set_point_position(1,(moon.position-mouseCurrentPosition).normalized() * magnituge * LINE_LENGTH)
 			for point in points:
 				line_2d.add_point(point)
